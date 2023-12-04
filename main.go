bd555cc3978a4e19db34a52eb36a6e49e649cfb4
Emil Djupvik <emil14833@gmail.com>
2023-12-04 10:47:48 +0100

    remove unused files

--- main.go
+++ main.go
@@ -262,10 +262,8 @@ func selectedUrlHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	// URL to make the GET request
 	url := "https://catfact.ninja/fact"
 
-	// Make the GET request
 	response, err := http.Get(url)
 	if err != nil {
 		fmt.Println("Error making GET request:", err)
@@ -273,14 +271,12 @@ func main() {
 	}
 	defer response.Body.Close()
 
-	// Read the response body
 	body, err := ioutil.ReadAll(response.Body)
 	if err != nil {
 		fmt.Println("Error reading response body:", err)
 		return
 	}
 
-	// Print the response to the console
 	fmt.Println("Response from", url, ":\n", string(body))
 
 	client = db.NewClient()
